295beb99c695d2835c33d14ce4a3e2593f7d7448
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-11 00:27:03 -0500

    Added random.js | potion.js test files

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,10 @@
+
+
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
