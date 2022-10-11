d8cfc3270b8d90b6f44b04d89d130a215bfe755f
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-11 01:15:02 -0500

    Updated All Lesson 10.1 Code

---
+++ __tests__/random.test.js
@@ -0,0 +1,6 @@
+const randomNumber = require('../lib/random.js');
+
+test('generates a random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
